b93ceeb40215e23ddc18676125c60eceec09425b
Bastien-Labastie <98860146+Bastien-Labastie@users.noreply.github.com>
2025-02-27 17:55:23 -0500

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -21,6 +21,7 @@ const { BadRequestError } = require("../expressError");
 
 router.post("/token", async function (req, res, next) {
   try {
+    alert("Hello");
     const validator = jsonschema.validate(req.body, userAuthSchema);
     if (!validator.valid) {
       const errs = validator.errors.map(e => e.stack);
